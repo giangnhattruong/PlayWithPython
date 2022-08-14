48d4ba788d70a6e7c1db4d8a16bfaaa541789207
giangnhattruong <giangnhattruong@gmail.com>
2022-08-14 18:04:12 +0700

    Add comment

--- Socket/Socket.py
+++ Socket/Socket.py
@@ -1,13 +1,19 @@
 import socket
 
+# Prepare socket and connect
 mySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 mySocket.connect(('data.pr4e.org', 80))
+
+# Send command
 cmd = 'GET http://data.pr4e.org/romeo.txt HTTP/1.0\n\n'.encode()
 mySocket.send(cmd)
 
+# Retrieve and print out data
 while True:
     data = mySocket.recv(512)
     if (len(data) < 1):
         break
     print(data.decode())
+    
+# Close socket
 mySocket.close()
--- WordCount/WordCount.py
+++ WordCount/WordCount.py
@@ -1,13 +1,14 @@
 name = input("Enter file name:")
 
+# Open file
 try:
     fileHandle = open(name)
 except:
     print("Cannot open file " + name + " . Please enter an existing file name.")
     quit()
 
+# Count words in file
 wordsCount = dict()
-
 for line in fileHandle:
     line = line.rstrip()
     if len(line) > 0:
@@ -15,12 +16,14 @@ for line in fileHandle:
         for word in words:
             wordsCount[word] = wordsCount.get(word, 0) + 1
 
+# Convert dictionary to list of tuples contain values and keys
 wordsCountList = list()
-
 for k, v in wordsCount.items():
-    wordsCountList.append((v, k))
+    wordsCountList.append((v, k)) # append tuple into list
 
+# Sort by count value desc
 sortedWordsCount = sorted(wordsCountList, reverse=True)
 
+# Print words count result
 for k, v in sortedWordsCount:
     print(str(k) + ": " + v)
